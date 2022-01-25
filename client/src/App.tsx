d8e5122a515dfc51fe75d1a14fa979c43b2c87ab
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-01-25 17:45:23 -0500

    add react-router-dom and created boilerplate for routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div className='App'>hi</div>;
+  return (
+    <Router>
+      <div className='App'>
+        {/* TODO ADD NAVBAR COMPONENT */}
+        <nav>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/Profile'}>Profile</Link>
+        </nav>
+        <main className='content'>
+          <Routes>
+            <Route path='/' element={<div>Home</div>} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
