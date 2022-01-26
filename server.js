01b31a46ccede931a939ecec373222cb20bcea77
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-01-25 21:20:06 -0500

    create jwt token when user is authenticated

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
 
 const PORT = 5000;
+
+app.use(express.json());
+
+const fakeData = [
+  {
+    username: 'Juan',
+    title: 'POST1',
+  },
+  {
+    username: 'Pablo',
+    title: 'POST2',
+  },
+];
+
+app.get('/data', (req, res) => {
+  res.json(fakeData);
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  //TODO: AUTHENTICATE USER
+  const user = { name: username };
+  const accesToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accesToken: accesToken });
+});
 
 app.listen(PORT, () => {
   console.log('Server listening in http://localhost:' + PORT);
