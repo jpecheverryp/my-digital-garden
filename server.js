9edb69736c1c194fbd37e5149c6868a581af813a
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-01-25 22:00:00 -0500

    implemented refresh tokens and expire access tokens

--- server.js
+++ server.js
@@ -19,17 +19,55 @@ const fakeData = [
   },
 ];
 
-app.get('/data', (req, res) => {
-  res.json(fakeData);
+// TODO: MOVE TO DATABASE
+let refreshTokens = [];
+
+app.get('/data', authenticateToken, (req, res) => {
+  res.json(fakeData.filter((thing) => thing.username === req.user.name));
+});
+
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken === null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken: accessToken });
+  });
+  return;
 });
 
 app.post('/login', (req, res) => {
   const username = req.body.username;
   //TODO: AUTHENTICATE USER
   const user = { name: username };
-  const accesToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({ accesToken: accesToken });
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user);
+  refreshTokens.push(refreshToken); //TODO: insert this token in database
+  res.json({ accessToken: accessToken, refreshToken: refreshToken });
 });
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '45m',
+  });
+}
+function generateRefreshToken(user) {
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+}
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token === null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(PORT, () => {
   console.log('Server listening in http://localhost:' + PORT);
