8a5e760210bc2b8fd9a75f09fb4d6b909c7deae6
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-01-29 16:59:52 -0500

    created refresh token model in deatabase

---
+++ models/RefreshToken.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Token extends Model {}
+
+Token.init(
+  {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'token',
+  }
+);
+
+module.exports = Token;
