0f89aa2760ae42cb2344fa03a303043eaa8df095
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-01-29 16:44:30 -0500

    test data route from client

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,19 +1,36 @@
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { redirectTo } from '../utils/redirectTo';
 interface IState {
   isAuthenticated: boolean;
 }
 const Home: React.FC<IState> = ({ isAuthenticated }) => {
   let navigate = useNavigate();
-  async function redirectToLogin() {
-    navigate('/login');
-  }
+
   useEffect(() => {
     if (!isAuthenticated) {
-      redirectToLogin();
+      redirectTo(navigate, '/login');
+      return;
     }
-  });
-  return <h1>Home</h1>;
+    axios({
+      method: 'GET',
+      url: '/data',
+      headers: {
+        'x-auth-token':
+          'BEARER eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImUyYzYzMmE1LTJiN2MtNGQ2Mi1hNTFmLTljZTczMmYyOGJmNyIsImlhdCI6MTY0MzQ5MDEyMSwiZXhwIjoxNjQzNDkyODIxfQ.65a3PKG7YhUYtKv1EoZN-yncO_T4t_uQ4N-Yz8pn4wQ',
+      },
+    })
+      .then((data) => console.log(data.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <section>
+      <h1>Home</h1>
+      {/* <p>{fakeData}</p> */}
+    </section>
+  );
 };
 
 export default Home;
