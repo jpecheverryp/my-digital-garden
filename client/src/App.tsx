b59c8961c58261d8c5d85a02fbf0cdf0dacc8ef0
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-02-11 00:29:52 -0500

    moved note view from components to screens

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,14 @@ import { Route, Routes, useNavigate } from 'react-router-dom';
 // Libraries and utilities
 import axios from 'axios';
 import { getRefreshToken, setAccessToken } from './utils/cookiesHandler';
-import { redirectTo } from './utils/redirectTo';
+
 // Components
 import Navbar from './components/Navbar';
 import Home from './screens/Home';
 import Login from './screens/Login';
 import Profile from './screens/Profile';
 import Register from './screens/Register';
-import NoteView from './components/NoteView';
+import NoteView from './screens/NoteView';
 import { Box } from '@chakra-ui/react';
 
 function App() {
