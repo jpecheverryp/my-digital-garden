97008f666421ecd3dbfed6967a3879d72d54ee2f
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-02-12 01:14:34 -0500

    moved node routes just before react routes

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(require('./routes'));
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+
+app.use(require('./routes'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
