aee1366a8fa4bb79b7b0e63d678cabe580453760
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-02-12 02:07:08 -0500

    changed line that serves react server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(require('./routes'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 sequelize.sync({ force: false }).then(() => {
