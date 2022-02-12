502a3c6b76be7b275fc2920755185ce504f868ac
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-02-12 00:26:40 -0500

    getting ready to deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const sequelize = require('./config/connection');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
