86d7e078c6b76748d0d26286d0ec25119edb35c1
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-02-12 01:08:07 -0500

    removed app.get from if statement

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
-  });
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+});
 
 app.use(require('./routes'));
 
